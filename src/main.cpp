2fae00780f6675c7302b07e624ff7028faf48632
ThetaSinner <greatdjonfire@hotmail.co.uk>
2016-11-02 11:32:37 +0000

    add data dump methods

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
-#include <echelon/parser/tokenizer.hpp>
-
 #include <string>
 #include <set>
 #include <list>
 #include <algorithm>
+#include <vector>
 
 #include <echelon/parser/tokenizer.hpp>
+#include <echelon/parser/token.hpp>
+#include <echelon/parser/token-type-enum.hpp>
+
+std::string toString(TokenTypeEnum tokenType) {
+  switch (tokenType) {
+    case TokenTypeEnum::String: return "string";
+    case TokenTypeEnum::Identifier: return "identifier";
+    case TokenTypeEnum::Integer: return "integer";
+    case TokenTypeEnum::BlockDelimO: return "block delim open";
+    case TokenTypeEnum::BlockDelimC: return "block delim close";
+    case TokenTypeEnum::Assign: return "assign";
+    default: return "none";
+  }
+}
+
+void stream_dump(std::ostream& s, std::vector<Token> tokens) {
+  for (auto& i : tokens) {
+    s << toString(i.getTokenType()) << " ["<< i.getData() << "], ";
+  }
+}
 
 int main(int argc, char** args) {
+  Tokenizer t;
+  auto r = t.tokenize("module {\n    int t = 1\n}");
+
+  stream_dump(std::cout, r);
+
   return 0;
 }
