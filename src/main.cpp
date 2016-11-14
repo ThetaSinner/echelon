c3f3f30bf18b64a0dac84b1654c8a6723fb2e45c
ThetaSinner <greatdjonfire@hotmail.co.uk>
2016-11-14 15:27:37 +0000

    clean up by removing dead code

--- src/main.cpp
+++ src/main.cpp
@@ -307,107 +307,6 @@ void stream_dump(std::ostream& s, std::vector<Token> tokens) {
   }
 }
 
-class ParserData {
-private:
-  std::map<Keyword, std::string> keywordStringMap;
-  std::set<std::string> keywordStringSet;
-  std::set<std::string> dataTypeIdentifierKeywordStringSet;
-public:
-  ParserData() {
-    keywordStringMap.insert({Keyword::Integer, "integer"});
-
-    for (auto& i : keywordStringMap) {
-      keywordStringSet.insert(i.second);
-    }
-
-    dataTypeIdentifierKeywordStringSet.insert(keywordStringMap.at(Keyword::Integer));
-  }
-
-  std::set<std::string>* getKeywordStringSet() {
-    return &keywordStringSet;
-  }
-
-  std::set<std::string>* getDataTypeIdentifierKeywordStringSet() {
-    return &dataTypeIdentifierKeywordStringSet;
-  }
-
-} parserData;
-
-// scrap.
-std::string toString(Keyword keyword) {
-  switch (keyword) {
-    case Keyword::Module: return "module";
-    case Keyword::Integer: return "integer";
-    default: return "none";
-  }
-}
-
-bool isDataTypeIdentifier(Token t) {
-  if (t.getTokenType() == TokenTypeEnum::Identifier) {
-    return parserData.getDataTypeIdentifierKeywordStringSet() -> find(t.getData())
-      != parserData.getDataTypeIdentifierKeywordStringSet() -> end();
-  }
-
-  return false;
-}
-
-bool isKeyword(Token t) {
-  if (t.getTokenType() == TokenTypeEnum::Identifier) {
-    return parserData.getKeywordStringSet() -> find(t.getData())
-      != parserData.getKeywordStringSet() -> end();
-  }
-
-  return false;
-}
-
-// scrap
-void process_dataTypeIdentifier(AstNode *node, std::vector<Token>::iterator it) {
-  AstNode* process_node = new AstNode();
-  process_node -> setType(AstNodeType::AssignmentExpr);
-
-  auto dataType = *it;
-  AstNode* type_node = new AstNode();
-  type_node -> setType(AstNodeType::ExplicitType);
-
-  it++;
-  auto identifier = *it;
-  std::cout << "identifier " << identifier.getData() << "\n";
-  if (identifier.getTokenType() != TokenTypeEnum::Identifier) {
-    std::cout << "expected identifier\n";
-  }
-  if (isKeyword(identifier)) {
-    std::cout << "cannot use keyword as identifier\n";
-  }
-  // add identifier to tree.
-
-  it++;
-  auto assign = *it;
-  std::cout << "assign " << assign.getData() << "\n";
-  if (assign.getTokenType() != TokenTypeEnum::Assign) {
-    std::cout << "expected equals sign\n";
-  }
-  // modify tree.
-
-  it++;
-  if (it -> getTokenType() == TokenTypeEnum::Integer) {
-    // add value to tree.
-  }
-  else {
-    std::cout << "expected value";
-  }
-}
-
-AstNode* parse(std::vector<Token> tokens) {
-  AstNode *node = new AstNode();
-
-  for (auto i = tokens.begin(); i != tokens.end(); i++) {
-    if (isDataTypeIdentifier(*i)) {
-      std::cout << i -> getData() << " is data type identifier\n";
-      process_dataTypeIdentifier(node, i);
-    }
-  }
-}
-
 bool is_letter(int c) {
   return (c >= 97) && (c <= 122);
 }
@@ -768,15 +667,6 @@ public:
 };
 
 int main(int argc, char** args) {
-  Tokenizer t;
-  auto r = t.tokenize("module {\n    integer t = 1\n}");
-
-  stream_dump(std::cout, r);
-
-  //auto x = parse(r);
-
-  //
-
   if (Keyword::Integer == "integer") {
     std::cout << "wow.\n";
   }
