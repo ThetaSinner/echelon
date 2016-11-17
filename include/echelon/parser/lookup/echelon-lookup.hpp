fc54b248cd4a20293e0021015e10ac707ffbb60e
ThetaSinner <greatdjonfire@hotmail.co.uk>
2016-11-17 19:41:23 +0000

    add files missed in last commit

---
+++ include/echelon/parser/lookup/echelon-lookup.hpp
@@ -0,0 +1,30 @@
+#ifndef ECHELON_LOOKUP_HPP_INCLUDED
+#define ECHELON_LOOKUP_HPP_INCLUDED
+
+#include <set>
+#include <string>
+
+class EchelonLookup {
+private:
+  static EchelonLookup *self;
+
+  std::set<std::string> dataTypeKeywordSet;
+  std::set<std::string> keywordSet;
+
+  EchelonLookup() {}
+  EchelonLookup(const EchelonLookup& _) {}
+  void operator=(const EchelonLookup& _) {}
+public:
+  static EchelonLookup* getInstance();
+
+  template<typename T>
+  static std::string toString(T t);
+
+  void addDataTypeKeyword(std::string dataTypeKeyword);
+  bool isDataTypeKeyword(std::string str);
+
+  void addKeyword(std::string keyword);
+  bool isKeyword(std::string str);
+};
+
+#endif
---
+++ src/parser/lookup/echelon-lookup.cpp
@@ -0,0 +1,58 @@
+#include <echelon/parser/lookup/echelon-lookup.hpp>
+
+#include <echelon/parser/token-type-enum.hpp>
+#include <echelon/ast/AstNodeType.hpp>
+
+EchelonLookup* EchelonLookup::self = nullptr;
+
+EchelonLookup* EchelonLookup::getInstance() {
+  if (self == nullptr) {
+    self = new EchelonLookup();
+  }
+
+  return self;
+}
+
+void EchelonLookup::addDataTypeKeyword(std::string dataTypeKeyword) {
+  dataTypeKeywordSet.insert(dataTypeKeyword);
+
+  keywordSet.insert(dataTypeKeyword);
+}
+
+bool EchelonLookup::isDataTypeKeyword(std::string str) {
+  return dataTypeKeywordSet.find(str) != dataTypeKeywordSet.end();
+}
+
+void EchelonLookup::addKeyword(std::string keyword) {
+  keywordSet.insert(keyword);
+}
+
+bool EchelonLookup::isKeyword(std::string str) {
+  return keywordSet.find(str) != keywordSet.end();
+}
+
+template<> std::string EchelonLookup::toString(TokenTypeEnum t) {
+  switch(t) {
+    case TokenTypeEnum::Identifier:
+      return "identifier";
+    case TokenTypeEnum::BlockDelimO:
+      return "block delim open";
+    case TokenTypeEnum::BlockDelimC:
+      return "block delim close";
+    default:
+      return "none";
+  }
+}
+
+template<> std::string EchelonLookup::toString(AstNodeType t) {
+  switch(t) {
+    case AstNodeType::Program:
+      return "program";
+    case AstNodeType::Package:
+      return "package";
+    case AstNodeType::Module:
+      return "module";
+    default:
+      return "none";
+  }
+}
