3b092059d6f87e92c6343fc93003e5f795b04295
ThetaSinner <greatdjonfire@hotmail.co.uk>
2016-12-20 11:40:43 +0000

    help cmake out with its dependency resolution
    now have a working test for the compiler

--- test/compiler-spider-monkey-js-test-suite.hpp
+++ test/compiler-spider-monkey-js-test-suite.hpp
@@ -1,19 +1,19 @@
 #include <cxxtest/TestSuite.h>
 
-//#include <echelon/code-generation/code-generator.hpp>
+#include <echelon/code-generation/code-generator.hpp>
 #include <echelon/compiler/echelon-compiler.hpp>
-//#include "../src/code-generation/spider-monkey-js/spider-monkey-js-code-generator.hpp"
+#include "../src/code-generation/spider-monkey-js/spider-monkey-js-code-generator.hpp"
 
 class CompilerSpiderMonkeyJSTestSuite : public CxxTest::TestSuite {
-    //EchelonCompiler compiler;
+    EchelonCompiler compiler;
 public:
     CompilerSpiderMonkeyJSTestSuite() {
-      //compiler.setCodeGenerator(new SpiderMonkeyJSCodeGenerator());
+      compiler.setCodeGenerator(new SpiderMonkeyJSCodeGenerator());
     }
 
     void testVariableDeclaration(void) {
-      //auto result = compiler.compile("string my_var = \"my string\"");
+      auto result = compiler.compile("string my_var = \"my string\"");
 
-      //TS_ASSERT_EQUALS("var my_var = \"my string\";", result);
+      TS_ASSERT_EQUALS("var my_var = \"my string\";", result);
     }
 };
